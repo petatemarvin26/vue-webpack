343b0ec0f3b279477099297ded6c7b6571761b80
Marvin Petate <46154422+petatemarvin26@users.noreply.github.com>
2023-08-20 14:00:06 +0800

    release/1.0.5 (#4)
    * release/1.0.5
    
    * release/1.0.5

--- module.d.ts
+++ module.d.ts
@@ -1,13 +1,1 @@
-declare module '*.json';
-
-declare module '*.module.css';
-declare module '*.css';
-
-declare module '*.gif';
-declare module '*.png';
-declare module '*.jpeg';
-declare module '*.jpg';
-
-declare module '*.vue';
-
 declare module '*';
